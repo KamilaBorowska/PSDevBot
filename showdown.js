b48a7c9d3c0f7ae571da777df437a93a57342e05
Insist <DeathlyPlays@users.noreply.github.com>
2017-10-19 09:51:15 -0500

    Cleanup

--- showdown.js
+++ showdown.js
@@ -41,7 +41,7 @@ Showdown.keys = {
     nickname: true,
     password: true,
     room: true,
-}
+};
 
 Showdown.commands = new Map([
     ['challstr', 'finalize'],
@@ -51,26 +51,26 @@ Showdown.commands = new Map([
 Showdown.prototype = new EventEmitter;
 
 Showdown.prototype.connect = function connect() {
-    var connection = new WebSocketClient
+    var connection = new WebSocketClient;
     connection.on('connectFailed', this.onConnectionFailure.bind(this));
     connection.on('connect', this.onConnect.bind(this));
 
     var connectionString = this.getConnectionString();
     connection.connect(connectionString, []);
-}
+};
 
 Showdown.prototype.getConnectionString = function getConnectionString() {
     return format(
         'ws://{}:{}/showdown/websocket',
         this.server,
-        this.serverport,
-    )
-}
+        this.serverport
+    );
+};
 
 Showdown.prototype.onConnectionFailure = function onConnectionFailure(error) {
     console.error('Error occured (%s), will connect in a minute', error);
     setTimeout(this.connect.bind(this), MINUTE);
-}
+};
 
 Showdown.prototype.onConnect = function onConnect(connection) {
     this.connection = connection;
@@ -80,12 +80,12 @@ Showdown.prototype.onConnect = function onConnect(connection) {
     connection.on('close', onConnectionFailure);
     connection.on('message', this.onMessage.bind(this));
     console.info('Connected to Showdown server');
-}
+};
 
 Showdown.prototype.onMessage = function onMessage(message) {
     if (message.type !== 'utf8') return;
     this.parseMessage(message.utf8Data);
-}
+};
 
 Showdown.prototype.parseMessage = function parseMessage(message) {
     console.log(message);
@@ -93,7 +93,7 @@ Showdown.prototype.parseMessage = function parseMessage(message) {
     if (Showdown.commands.has(parts[1])) {
         this[Showdown.commands.get(parts[1])](parts);
     }
-}
+};
 
 Showdown.prototype.finalize = function finalize(parts) {
     var id = parts[2];
@@ -111,28 +111,28 @@ Showdown.prototype.finalize = function finalize(parts) {
                 pass: password,
             }
         },
-        function finish(error, response, body) {
+        function finish(body) {
             var result = JSON.parse(body.replace(/^]/, ""));
             var assertion = result.assertion;
             var command = format('/trn {},0,{}', nickname, assertion);
             this.report(command);
             this.report('/join ' + this.room);
             this.report('/away');
-        }.bind(this);
+        }.bind(this)
     );
-}
+};
 
 Showdown.prototype.onChatMessage = function onChatMessage(parts) {
     this.emit('message', parts[3], parts.slice(4).join('|'));
-}
+};
 
 Showdown.prototype.report = function report(message) {
     this.queue = this.queue.then(() => {
         this.connection.send(`${this.room}|${message}`);
         return new Promise(resolve => {
             setTimeout(resolve, 500);
-        })
-    })
-}
+        });
+    });
+};
 
 module.exports = Showdown;
